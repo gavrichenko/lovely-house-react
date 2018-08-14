8333eb69dcb5937689642e43aa2ddec125ac31ca
ilya <gavr.imba@mail.ru>
2018-08-14 04:34:14 +0300

    add links for nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,84 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import { Menu } from 'semantic-ui-react'
 import './Header.css'
-import Logo from '../Logo';
+
 
 class Header extends Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
+    const { activeItem } = this.state
+
     return (
       <header className="header">
+        <Menu stackable>
 
-        <div className="header__logo">
-            <Logo />
-        </div>
-        <div className="header__links">
-          <ul>
-            <li><a href="#">Акции</a></li>
-            <li><a href="#">Контакты</a></li>
-            <li><a href="#">Купить</a></li>
-          </ul>
-        </div>
-        <div className="header__basket">
-          Корзина(иконка)
-        </div>
+          <Link to="/">
+            <Menu.Item>
+              <img src={require('../../static/ico/flowers.png')} />
+              <span className="nameOfLogo"><p>lovely-house</p></span>
+            </Menu.Item>
+          </Link>
 
+          <Link to="/promotion">
+            <Menu.Item
+              name='promo'
+              active={activeItem === 'promo'}
+              onClick={this.handleItemClick}
+            >
+              Акции
+            </Menu.Item>
+          </Link>
+
+          <Link to="/contacts">
+            <Menu.Item
+              name='contacts'
+              active={activeItem === 'contacts'}
+              onClick={this.handleItemClick}
+            >
+              Контакты
+            </Menu.Item>
+          </Link>
+
+          <Menu.Item name='sign-in' active={activeItem === 'sign-in'} onClick={this.handleItemClick}>
+            Купить
+          </Menu.Item>
+        </Menu>
       </header>
     )
   }
+  // render() {
+  //   return (
+  //     <header className="header">
+  //
+  //       <div className="header__logo">
+  //           <Logo />
+  //       </div>
+  //
+  //       <div className="header__buttons">
+  //         <Button.Group color='black'>
+  //           <Button>Акции</Button>
+  //           <Button>Контакты</Button>
+  //           <Button>Купить</Button>
+  //         </Button.Group>
+  //       </div>
+  //       <div className="header__links">
+  //         <ul>
+  //           <li><a href="#">Акции</a></li>
+  //           <li><a href="#">Контакты</a></li>
+  //           <li><a href="#">Купить</a></li>
+  //         </ul>
+  //       </div>
+  //       <div className="header__basket">
+  //         Корзина(иконка)
+  //       </div>
+  //
+  //     </header>
+  //   )
+  // }
 
 }
 
